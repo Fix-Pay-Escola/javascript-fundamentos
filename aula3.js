59f44c441f61ef917c6d8bfd07250b7a22970718
Lucas-Isabel <51051616+LuqFP@users.noreply.github.com>
2023-08-11 16:52:09 -0300

    funções

--- aula3.js
+++ aula3.js
@@ -1,19 +1,22 @@
 //funções
 function maioridade(idade){
-    if(idade < 18){
-        return False
+    if(idade > 18){
+        return maioridade = true
     }
-    if(idade >= 18){
-        return True
+    else{
+        return maioridade = false
+    }
+}
+function checando_maioridade(){
+    if (maioridade === true){
+        return console.log("venda autorizada ")
+    }
+    else{
+        return console.log("venda não autorizada")
     }
 }
 console.log("checando a idade do cliente")
 
-
-if(maioridade(16)){
-
-    console.log("cliente com maioridade compra de bebidas alcoolicas autorizada")
-}
-else{
-    console.log("cliente menor de idade compra de bebidas alcoolicas negada")
-}
+let  idade = 90;
+maioridade(idade);
+checando_maioridade()
