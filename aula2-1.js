76468c72b5217ec4f064cbd4d8f189c86068313e
Lucas-Isabel <51051616+LuqFP@users.noreply.github.com>
2023-08-11 15:10:26 -0300

    exemplo de comparações

---
+++ aula2-1.js
@@ -0,0 +1,7 @@
+//tipos de comparações
+const num1 = 456
+const num2 = "456"
+
+console.log(num1 == num2) //dois sinais de igualdade comparam o conteudo retornando true nesse caso
+
+console.log(num1 === num2)// três sinais de igualdade comparam o tipo e o conteudo retornando false nesse caso
