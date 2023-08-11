1fb6fe1c001a1ca9219c8f69a84dff21ec4973a4
Lucas-Isabel <51051616+LuqFP@users.noreply.github.com>
2023-08-11 14:49:10 -0300

    experimento com if-else e true or false

---
+++ aula2-3.js
@@ -0,0 +1,17 @@
+//IF-ELSE
+//TRUE-FALSE
+
+let base = 1
+let altura = 0
+let area
+
+if(base === 0 || altura === 0){
+    console.log("quadrado inexistente")
+}else{
+    area = base * altura
+    if(area > 10){
+        console.log(`a area desse quadrado é ${area}, ou seja, maior que 10`)
+    }else{
+        console.log(`a area desse quadrado é ${area}, ou seja, menor que 10`)
+    }
+}
